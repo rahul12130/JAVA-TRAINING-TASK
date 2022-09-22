5483d55b7cf8a552ed7de5a343bf2cc01c5a60e3
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-09-22 12:29:59 +0530

    Add files via upload

---
+++ overiding.java
@@ -0,0 +1,59 @@
+class Person {
+  int id;
+  String name;
+  String phone;
+  
+
+  Person(int id, String phone, String name) {
+    this.id=id;
+    this.name = name;
+    this.phone= phone;
+  }
+  void show(String person1){
+    System.out.println("Employee brother name: "+person1);
+  }
+}
+
+class Employee extends Person {
+  
+  int emp_salary;
+
+  Employee(int id, String name, String phone, int salary) {
+    super(id, phone, name);
+    this.emp_salary = salary;
+  }
+
+
+  void printEmployeeDetails() {
+    System.out.println("Employee ID     :  " + id);
+    System.out.println("Employee Name   :  " + name);
+    System.out.println("Employee phone   :  " + phone);
+    System.out.println("Employee salary :  "+ emp_salary);
+  }
+
+  void printEmployeeDetails(int tax) {
+    System.out.println("Employee salary :  "+ (emp_salary-tax));
+    System.out.println("Tax deducted :  "+ tax);
+  }
+  void show(String person2){
+    System.out.println("Employee brother name: "+person2);
+  }
+
+}
+
+public class jp3 {
+  public static void main(String[] args) {
+    Person p = new Person(646, "9686515970", "Rahul Chavan");
+    Employee emp = new Employee(646, "9686515970", "Rahul Chavan", 45000);
+     
+
+     //method overloading
+    emp.printEmployeeDetails();
+    emp.printEmployeeDetails(5000);
+
+    //method overriding
+    emp.show("Shankar");
+    p.show("Rohit");
+
+  }
+}
