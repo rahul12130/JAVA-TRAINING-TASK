87c489824d3ab93bc4b79609ef737b4993de3939
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-09-22 16:24:59 +0530

    Add files via upload

---
+++ addtype.java
@@ -0,0 +1,40 @@
+class A{
+	int id;
+	String name;
+
+	public A(int i, String n){
+
+		this.id = id;
+		this.name= n;
+	}
+	
+
+		void Display() {
+			if(name=="HR") {
+				System.out.println("HR");
+			}
+			else {
+				System.out.println("not HR");
+		}
+}
+
+
+
+void show() {
+	switch(name) {
+		case "HR" : System.out.println("HR");
+		break;
+			case "Not HR" : System.out.println("Not HR");
+		break;
+			default  : System.out.println("NA");
+		break;
+		}
+	
+}
+class empdata {
+	
+		public static void main(String[] args) {
+			A obj = new A(1,"HR");
+			obj.Display();
+			obj.show();}}}
+		
