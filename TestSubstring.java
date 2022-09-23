ff919bd8d4b223bdb990ef64fe4fd7e9b9d5efc3
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-09-23 16:18:39 +0530

    Add files via upload

---
+++ TestSubstring.java
@@ -0,0 +1,7 @@
+    public class TestSubstring{    
+     public static void main(String args[]){    
+     String s="i have joined at amantya at 01-september-2022";    
+     System.out.println("Original String: " + s);  
+     System.out.println("Substring starting from index 28 to 45: " +s.substring(28,45));
+     }  
+    }    
