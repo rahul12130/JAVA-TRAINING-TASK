2d82bf1ab405e8c68e9bf6289af37d4baa571b9d
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-09-26 22:31:51 +0530

    Add files via upload

---
+++ basicregex.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("rahul.chavan0@gmail.com");
+        boolean matchFound = matcher.find();
+        if(matchFound) {
+            System.out.println("Match found");
+        } else {
+            System.out.println("Match not found");
+        }
+    }
+}
