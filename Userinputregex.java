df0334dde5bfc97ca67025fe102f9bd46b0ba914
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-09-27 14:00:50 +0530

    Update Userinputregex.java

--- Userinputregex.java
+++ Userinputregex.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Userinputregex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your Email: ");
