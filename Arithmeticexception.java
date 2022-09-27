4a66f763f66f3af30491b85863b179b8a73149a9
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-09-27 17:20:20 +0530

    Add files via upload

---
+++ Arithmeticexception.java
@@ -0,0 +1,15 @@
+public class Arithmeticexception {
+    public static void main(String args[]){
+
+        System.out.println("Statement 1");
+        System.out.println("Statement 2");
+        System.out.println("Statement 3");
+        try{
+            int i = 10/0;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+        System.out.println("Statement 4");
+        System.out.println("Statement 5");
+    }
+}
