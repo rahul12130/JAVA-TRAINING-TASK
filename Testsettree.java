2d8c221e2c9d90f7d632ed370533da8be18514f2
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-10-09 09:56:27 +0530

    Add files via upload

---
+++ Testsettree.java
@@ -0,0 +1,18 @@
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Testsettree {
+    public static void main(String[] args) {
+        Set<String> set = new TreeSet();
+        set.add("ABC");
+        set.add("DEF");
+        set.add("GHI");
+        set.add("WXY");
+        set.add("JKL");
+        set.add("MNO");
+        set.add("RST");
+        set.add("PQL");
+        System.out.println(set);
+    }
+}
