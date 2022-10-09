ca9e533206f75ca5dcd6ac299775ddcc358af873
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-10-09 22:42:53 +0530

    Add files via upload

---
+++ IncDeccount.java
@@ -0,0 +1,33 @@
+class Myclass extends Thread{
+    static int count=0;
+    void inc(){
+        count++;
+    }
+    void dec(){
+
+        try { Thread.sleep(100);
+            count--;
+        }
+        catch(Exception e) {
+        }
+    }
+    public void run() {
+        inc();
+        System.out.println(count+"-"+Thread.currentThread().getName());
+        dec();
+        System.out.println(count+"-"+Thread.currentThread().getName());
+    }
+}
+public class IncDeccount {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Myclass t1= new Myclass();
+        Myclass t2= new Myclass();
+        t1.start();
+        t2.start();
+//		try { Thread.sleep(100);}
+//		catch(Exception e) {
+//			}
+    }
+
+}
