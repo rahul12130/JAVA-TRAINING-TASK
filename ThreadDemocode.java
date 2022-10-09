9bf30a72e5082d6fe7edf01e19f7ca1cff9750be
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-10-09 22:20:54 +0530

    Add files via upload

---
+++ ThreadDemocode.java
@@ -0,0 +1,31 @@
+class MainThreadd extends Thread{
+    int i;
+    public void run(){
+        for (i=1;i<=5 ;i++ ) {
+            System.out.println(i);
+        }
+
+    }
+}
+class ChildThreadd extends Thread{
+    int i;
+    public void run(){
+        for (i=1;i<=5 ;i++ ) {
+            System.out.println(i);
+        }
+
+    }
+}
+class ThreadDemo{
+    public static void main(String[] args) throws InterruptedException {
+        MainThreadd obj1 = new MainThreadd();
+        ChildThreadd  obj2 = new ChildThreadd ();
+
+
+
+        obj1.start();
+        obj1.join();
+        obj2.start();
+
+    }
+}
