e7a2394a9b37f173c45f2bc07040cc4fcade9bc8
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-10-09 22:38:00 +0530

    Add files via upload

---
+++ Runnablethreadimplement.java
@@ -0,0 +1,31 @@
+class MainnThreadd implements Runnable{
+    int i;
+    public void run(){
+        for (i=1;i<=5;i++ ) {
+            System.out.println(i);
+        }
+
+    }
+}
+class ChilddThreadd implements Runnable{
+    int i;
+    public void run(){
+        for (i=1;i<=5;i++ ) {
+            System.out.println(i);
+        }
+    }
+}
+class Runnablethreadimplement {
+    public static void main(String[] args) throws InterruptedException {
+        MainnThreadd obj1 = new MainnThreadd();
+        ChilddThreadd  obj2 = new ChilddThreadd ();
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t1.join();
+        t2.start();
+
+    }
+}
