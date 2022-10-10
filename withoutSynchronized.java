02f6dc62d76535435a2fb12bffa9f7614a25681a
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-10-10 22:09:39 +0530

    Add files via upload

---
+++ withoutSynchronized.java
@@ -0,0 +1,46 @@
+public class withoutSynchronized {
+    int count = 0;
+    void Increment() throws InterruptedException {
+        count++;
+    }
+    public static void main(String[] args) throws InterruptedException {
+        withoutSynchronized ds = new withoutSynchronized();
+
+        class T1 extends Thread{
+            @Override
+            public void run() {
+                int i;
+                try {
+                    for ( i=0; i<1000; i++){
+                        ds.Increment();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        class T2 extends Thread{
+            @Override
+            public void run() {
+                int i;
+                try {
+                    for (i=0; i<1000; i++){
+                        ds.Increment();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        T1 t1 =new T1();
+        T2 t2 =new T2();
+
+        t1.start();
+        t2.start();
+        Thread.sleep(1000);
+        System.out.println(ds.count);
+
+    }
+}
