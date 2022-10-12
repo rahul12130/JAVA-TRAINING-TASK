bd6c283cb74863f667b2cacb66d9701229f16fc2
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-10-12 13:22:46 +0530

    Update Userinputregex.java

--- Userinputregex.java
+++ Userinputregex.java
@@ -3,10 +3,9 @@ public class Userinputregex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your Email: ");
-        String phone = sc.next();
+        String Email = sc.next();
         String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-        //Matching the given phone number with regular expression
-        boolean result = phone.matches(regex);
+        boolean result = Email.matches(regex);
         if(result) {
             System.out.println("Given email-id is valid");
         } else {
