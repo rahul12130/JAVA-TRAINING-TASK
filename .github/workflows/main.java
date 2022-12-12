d421ae3b2d1320738364cc37006e282a6b6662c6
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-12-12 11:53:11 +0530

    Create main.java

---
+++ .github/workflows/main.java
@@ -0,0 +1,10 @@
+
+class ReverseString {
+    public static void main(String[] args) {
+     int i=0;
+        String len = "Rahul Chavan";
+        for (i =len.length()-1; i>=0; i--) {
+            System.out.print(len.charAt(i));
+        }
+    }
+}
